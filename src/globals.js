8cee775ac014934359805aa6f39e3eea033b6f26
James Serengia <jamesserengia@gmail.com>
2023-02-28 06:15:12 +0300

    Implement createAsyncThunks for fetching and adding book to API

---
+++ src/globals.js
@@ -0,0 +1,4 @@
+const API_URL =
+  "https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/XMpcykCejhyFfnA1ddnb";
+
+export default API_URL;
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,6 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import API_URL from "../../globals";
+
+export const postBook = createAsyncThunk(
+  "books/postBook",
+  async (bookData, thunkAPI) => {
+    try {
+      const res = await axios.post(API_URL, bookData);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error?.data?.message || "Something went wrong!"
+      );
+    }
+  }
+);
+
+export const getBooks = createAsyncThunk(
+  "books/postBook",
+  async (_, thunkAPI) => {
+    try {
+      const res = await axios(API_URL);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error?.data?.message || "Something went wrong!"
+      );
+    }
+  }
+);
 
 const initialState = {
+  isLoading: false,
   books: [
     {
       item_id: "item1",
@@ -39,6 +71,35 @@ const booksSlice = createSlice({
         (book) => book.item_id !== idOfBookToRemove
       );
     },
+  },
+  extraReducers: (builder) => {
+    // posting a book
+    builder
+      .addCase(postBook.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(postBook.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.books = state.books.push(action.payload);
+      })
+      .addCase(postBook.rejected, (state, action) => {
+        console.log(action);
+        state.isLoading = false;
+      });
+
+    // get books
+    builder
+      .addCase(getBooks.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getBooks.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.books = action.payload;
+      })
+      .addCase(getBooks.rejected, (state, action) => {
+        console.log(action);
+        state.isLoading = false;
+      });
   },
 });
 
